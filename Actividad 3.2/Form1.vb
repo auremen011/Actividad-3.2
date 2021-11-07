a910d1cd3d0855681ffa816dfac41d24733496c0
auremen011 <auremen011@g.educaand.es>
2021-11-07 16:54:25 +0100

    Cambio 2

--- Actividad 3.2/Form1.vb
+++ Actividad 3.2/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("hola. Este es mi primer programa")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("hola. Este es el segundo boton")
+    End Sub
 End Class
